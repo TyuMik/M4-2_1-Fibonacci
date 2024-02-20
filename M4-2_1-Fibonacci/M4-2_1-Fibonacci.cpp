ac115655222694620d01407c9c39c8a2165e7bff
Mikhail <miktyu88@gmail.com>
2024-02-20 22:17:19 +0300

    Initial commit

---
+++ M4-2_1-Fibonacci/M4-2_1-Fibonacci.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+long int Fib(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return (Fib(n - 1) + Fib(n - 2));
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+    do {
+        std::cout << "Введите число: ";
+        std::cin >> n;
+    } while (n < 0);
+    std::cout << "Число Фибоначчи под номером " << n << " равно " << Fib(n) << std::endl;
+    return 0;
+}
